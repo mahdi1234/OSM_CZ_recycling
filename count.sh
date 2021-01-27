f8091bda1fbbdd440594d573105ef361c4cd68e1
mahdi1234 <mahdi1234@centrum.cz>
2021-01-27 19:19:09 +0000

    Add files via upload

---
+++ count.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+touch progress.txt
+echo "Sberna strediska" >> ./progress.txt
+cat ./undefined_recycling_center.json | grep 'centre' | wc -l >> ./progress.txt
+echo "Kontejnery" >> ./progress.txt
+cat ./undefined_recycling_container.json | grep 'container' | wc -l >> ./progress.txt
+echo "Bez typu" >> ./progress.txt
+cat ./undefined_recycling_no_type.json | grep 'amenity' | wc -l >> ./progress.txt
