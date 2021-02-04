afebb985e61262548cf509f3ebf8ff9148543e2c
mahdi1234 <mahdi1234@centrum.cz>
2021-02-04 21:33:28 +0000

    Remove timestamps from jsons

--- umap.sh
+++ umap.sh
@@ -7,3 +7,4 @@ wget -O undefined_recycling_center.json 'https://overpass-api.de/api/interpreter
 echo "undefined_recycling_no_type"
 sleep 300
 wget -O undefined_recycling_no_type.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea(3600051684)-%3E.searchArea%3B(node%5B%22amenity%22%3D%22recycling%22%5D%5B%22recycling_type%22!%3D%22centre%22%5D%5B%22recycling_type%22!%3D%22container%22%5D%5B%22recycling_type%22!%3D%22underground%22%5D(area.searchArea)%3Bway%5B%22amenity%22%3D%22recycling%22%5D%5B%22recycling_type%22!%3D%22centre%22%5D%5B%22recycling_type%22!%3D%22container%22%5D%5B%22recycling_type%22!%3D%22underground%22%5D(area.searchArea)%3Brelation%5B%22amenity%22%3D%22recycling%22%5D%5B%22recycling_type%22!%3D%22centre%22%5D%5B%22recycling_type%22!%3D%22container%22%5D%5B%22recycling_type%22!%3D%22underground%22%5D(area.searchArea)%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
+sed -i '/.*timestamp_.*/d' *.json
