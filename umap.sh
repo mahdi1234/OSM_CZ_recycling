71a717d29cd5640c9ec911da5255c6d719fcd0aa
mahdi1234 <mahdi1234@centrum.cz>
2021-06-09 15:40:30 +0000

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -32,3 +32,7 @@ echo "undefined_recycling_no_type"
 wget -O undefined_recycling_no_type.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea(3600051684)-%3E.searchArea%3B(node%5B%22amenity%22%3D%22recycling%22%5D%5B%22recycling_type%22!~%22.*%22%5D(area.searchArea)%3Bway%5B%22amenity%22%3D%22recycling%22%5D%5B%22recycling_type%22!~%22.*%22%5D(area.searchArea)%3Brelation%5B%22amenity%22%3D%22recycling%22%5D%5B%22recycling_type%22!~%22.*%22%5D(area.searchArea)%3B)%3Bout%20center%20qt%3B'
 
 sed -i '/.*timestamp_.*/d' *.json
+
+osmtogeojson undefined_recycling_container.json >| undefined_recycling_container.geojson
+osmtogeojson undefined_recycling_center.json >| undefined_recycling_center.geojson
+osmtogeojson undefined_recycling_no_type.json >| undefined_recycling_no_type.geojson
